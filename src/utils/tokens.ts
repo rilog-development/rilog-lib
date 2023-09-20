5506d61d7bfb39c90c6b97ee4c0268f756526d9e
Andrii Karnaukh <kaowebdev@gmail.com>
2023-09-20 23:14:57 +0300

    remove token-generator lib from project | add new token generation function

--- src/utils/tokens.ts
+++ src/utils/tokens.ts
@@ -1,22 +1,21 @@
-import { RIL_TOKEN, TOKEN_GENERATION_SALT, TOKEN_GENERATION_TIMESTAMP } from '../constants';
+import { RIL_TOKEN } from '../constants';
+const generateToken = () => {
+    const rand = () => Math.random().toString(32).substr(2);
 
-/* tslint:disable:no-var-requires */
-const TokenGenerator = require('token-generator')({
-    salt: TOKEN_GENERATION_SALT,
-    timestampMap: TOKEN_GENERATION_TIMESTAMP, // 10 chars array for obfuscation proposes
-});
+    return rand() + rand() + rand();
+};
 /**
  * Set token for each not auth user for unique user registration
  * (here unique user mean open app in browser,
  * also user can enter self unique token is spec input for auth in another browser/device )
  */
 const getUserUniqToken = (): string => {
-    const savedToken = localStorage.getItem(RIL_TOKEN);
+    const savedToken = localStorage?.getItem(RIL_TOKEN);
 
     if (savedToken) {
         return savedToken;
     } else {
-        const token = TokenGenerator.generate();
+        const token = generateToken();
         localStorage.setItem(RIL_TOKEN, token);
 
         return token;
