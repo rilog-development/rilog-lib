363be62978e544bccddfbc47fd9f50582f2d750c
Andrii Karnaukh <kaowebdev@gmail.com>
2023-09-28 18:55:44 +0300

    add logs to fetch interceptor

--- src/adapters/fetch-adapter.ts
+++ src/adapters/fetch-adapter.ts
@@ -1,10 +1,12 @@
 import { TRilogPushRequest, TRilogPushResponse } from '../types';
 
 const fetchAdapterRequest = (data: TRilogPushRequest) => {
+    console.log("[fetchAdapterRequest] data", data);
     return data;
 };
 
 const fetchAdapterResponse = (data: TRilogPushResponse) => {
+    console.log("[fetchAdapterResponse] data", data);
     return data;
 };
 
