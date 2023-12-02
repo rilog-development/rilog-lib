738a96e07bded86fbadc7059ce628ff3fb2f61be
Andrii Karnaukh <kaowebdev@gmail.com>
2023-12-02 15:49:56 +0200

    upd

--- src/api/save.ts
+++ src/api/save.ts
@@ -11,8 +11,8 @@ type TSaveRequestResp = {
  */
 
 const saveRequest = (data: string, token: string): Promise<TSaveRequestResp> => {
-    console.log("[saveRequest] token ", token);
-    
+    console.log('[saveRequest] token ', token);
+
     return fetch(`${BASE_URL}/connection/send`, {
         method: 'POST',
         headers: {
